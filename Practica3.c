875c12335997b0c1bcdb6f2d755574f34cc5461b
Kpiiale <130101475+Kpiiale@users.noreply.github.com>
2023-07-06 17:16:55 -0500

    Update Practica3.c

--- Practica3.c
+++ Practica3.c
@@ -3,9 +3,9 @@
 int main (int argc, char *argv[]) {
     float base, altura;
 printf ("Introduzca la base del rectangulo");
-scanf ("&£", &base);
+scanf ("%f", &base);
 printf ("Introduzca la altura del rectangulo");
-scanf ("S[", &altura);
+scanf ("%f", &altura);
 float area=base * altura;
 printf("El area del rectangulo de base %.2f y alkura %.2f es: %.2f", base, altura, area); 
     return 0;
