baeb4f81ef0ebf68a09a780d98a48d07e53d94c5
Kpiiale <130101475+Kpiiale@users.noreply.github.com>
2023-07-06 17:39:37 -0500

    Update Practica15.c

--- Practica15.c
+++ Practica15.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-
-int main (int argc, char *argv[]) {
-    char cadena[50]; 
+int main(){
+    char cadena[50];
     char caracter;
-printf ("Introduce una cadena\n");
-gets(cadena);
-printf ("Introduce un caracter\n");
-caracter = getchar();
-int i = 0;
-int pos = -1;
-while (cadena[i] != '\0' && pos == -1){
-if (cadena [i] == caracter){
-pos = i;
+    printf("introduce una cadena");
+    gets(cadena);
+    printf("introduce un caracter");
+    caracter = getchar();
+    int i=0;
+    int pos=1;
+    while (cadena[i] !=0 )
+    {
+        if (cadena[i] == caracter){
+            pos=i;
+        }
+    }
+    if (pos != -1)
+    {
+        printf("El caracter %c se encuentra en la cadena %s en lo posicion %d",caracter,cadena,pos);
+    }
+    else{
+        printf("el caracter %c no se encuentra en la cadena %s",caracter,cadena);
+    }
+    
+
 }
-i + 1;
-}
-if (pos != -1){
-printf ("El caracter %c se encuentza en la cadena §s en la posición %d", caracter, cadena, pos);
-}
-else{
-    printf ("El caracter So no se ensuentra en la sadena %s", caracter, cadena) ;
-}
-    return 0;
-}
+
