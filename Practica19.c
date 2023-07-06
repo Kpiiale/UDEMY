ffebfb798267ed9dca4b5eb139a3a354f22848d6
Kpiiale <130101475+Kpiiale@users.noreply.github.com>
2023-07-06 17:13:06 -0500

    Update Practica19.c

--- Practica19.c
+++ Practica19.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
+    int numero; 
+    printf("Introduzca un numero:\n");
+    scanf("%d", &numero);
+    FILE *f;
+    f=fopen("tabla.txt", "w");
+    for (int i=0; i<=10; i++){
+        fprintf(f, "%d\n", i*numero); 
+    }
     return 0;
 }
