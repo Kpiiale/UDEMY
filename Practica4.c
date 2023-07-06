a31e6647d8c17aca7bd10d3eb8e3de32ad651a33
Kpiiale <130101475+Kpiiale@users.noreply.github.com>
2023-07-06 17:17:18 -0500

    Update Practica4.c

--- Practica4.c
+++ Practica4.c
@@ -3,8 +3,8 @@
 int main (int argc, char *argv[]) {
     float euros;
 printf ("Introduce les euros que quieres convertirin");
-scanf ("Sf" ,&euros);
+scanf ("%f" ,&euros);
 //pesetas = euros * 166.386
-printf ("8.21 euros equivalen a %.2f pesetas", euros, euros*166.386) ;
+printf ("%.2f euros equivalen a %.2f pesetas", euros, euros*166.386) ;
     return 0;
 }
